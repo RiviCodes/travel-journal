a9bd08c2f2924d530ace1520d7ae8f46740e7db6
RiviCodes <jose.riveramz@udlap.mx>
2022-04-29 15:38:16 -0500

    Complete Card component

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/Card.css";
+
+export default function Card(props) {
+  return (
+    <section className="card-container">
+      <div className="picture-container">
+        <img src={props.item.imageUrl} alt="location" />
+      </div>
+      <div>
+        <img
+          src={`../images/${props.item.locationIcon}`}
+          className="location-icon"
+        />
+        <span className="card--location">{props.item.location}</span>
+        <a
+          href={props.item.googleMapsUrl}
+          target="_blank"
+          className="card--mapsUrl"
+        >
+          View on Google Maps
+        </a>
+        <p className="card--title">{props.item.title}</p>
+        <span className="card--date">
+          {props.item.startDate} - {props.item.endDate}
+        </span>
+        <p className="card--description">{props.item.description}</p>
+      </div>
+    </section>
+  );
+}
