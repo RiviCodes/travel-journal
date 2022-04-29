cc31cdc77844389c89641018f7e99987361b2bed
RiviCodes <jose.riveramz@udlap.mx>
2022-04-29 15:59:45 -0500

    Include additional data

--- src/travelData.js
+++ src/travelData.js
@@ -9,7 +9,30 @@ export default [
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     locationIcon: "location.png",
-    imageUrl:
-      "https://i.pinimg.com/originals/da/4f/f4/da4ff49c91730d286f444110fb20c1ae.jpg",
+    imageUrl: "mount-fuji.jpg",
+  },
+  {
+    id: 2,
+    title: "Manhattan",
+    location: "New York",
+    googleMapsUrl: "https://goo.gl/maps/j5uQhCbJL18oh4gr7",
+    startDate: "20 Jun, 2016",
+    endDate: "20 Aug, 2016",
+    description:
+      "Manhattan is the most densely populated and geographically smallest of the five boroughs of New York City. It is the urban core of the New York metropolitan area, and coextensive with New York County.",
+    locationIcon: "location.png",
+    imageUrl: "manhattan.jpg",
+  },
+  {
+    id: 3,
+    title: "Florence",
+    location: "Italy",
+    googleMapsUrl: "https://goo.gl/maps/fpXVfmK4HvLjWdfs9",
+    startDate: "04 Jun, 2016",
+    endDate: "02 Jul, 2016",
+    description:
+      "Forence is a city in Central Italy and the capital city of the Tuscany region. It is the most populated city in Tuscany, with 383,084 inhabitants in 2013, and over 1,520,000 in its metropolitan area.",
+    locationIcon: "location.png",
+    imageUrl: "florence.jpg",
   },
 ];
