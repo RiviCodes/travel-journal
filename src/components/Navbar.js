8343c96c1ea5c0c52a944853203225483c66b6e8
RiviCodes <jose.riveramz@udlap.mx>
2022-04-29 15:37:00 -0500

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="/images/earth-globe.png" />
+      <h1>my travel journal</h1>
+    </nav>
+  );
+}
