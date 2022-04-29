3840aa44b9138748ed75755519b6d6e19b410ade
RiviCodes <jose.riveramz@udlap.mx>
2022-04-29 15:34:21 -0500

    Render Navbar & Card components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import travelData from "./travelData";
 
 function App() {
-  return <></>;
+  const cards = travelData.map((item) => {
+    return (
+      <>
+        <Card key={item.id} item={item} />
+        <hr />
+      </>
+    );
+  });
+
+  return (
+    <section className="app">
+      <Navbar />
+      {cards}
+    </section>
+  );
 }
 
 export default App;
